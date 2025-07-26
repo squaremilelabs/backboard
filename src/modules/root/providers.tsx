be8a4ecade57fd5dac460c241cf4facf4f888ddd
E <e@elzr.me>
2025-07-26 18:42:09 -0400

    add analytics

--- src/modules/root/providers.tsx
+++ src/modules/root/providers.tsx
@@ -5,19 +5,26 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import { RouterProvider } from "react-aria-components"
 import { useRouter } from "next/navigation"
 import { ThemeProvider as NextThemeProvider } from "next-themes"
+import { Suspense } from "react"
+import { Analytics } from "@vercel/analytics/next"
 import { InstantAccountListner } from "../account/instant-account"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient()
   const router = useRouter()
   return (
-    <ClerkProvider>
-      <QueryClientProvider client={queryClient}>
-        <NextThemeProvider attribute="class">
-          <RouterProvider navigate={router.push}>{children}</RouterProvider>
-        </NextThemeProvider>
-      </QueryClientProvider>
-      <InstantAccountListner />
-    </ClerkProvider>
+    <>
+      <Suspense>
+        <ClerkProvider>
+          <QueryClientProvider client={queryClient}>
+            <NextThemeProvider attribute="class">
+              <RouterProvider navigate={router.push}>{children}</RouterProvider>
+            </NextThemeProvider>
+          </QueryClientProvider>
+          <InstantAccountListner />
+        </ClerkProvider>
+      </Suspense>
+      <Analytics />
+    </>
   )
 }
