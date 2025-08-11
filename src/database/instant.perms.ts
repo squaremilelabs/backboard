d18f87a84676eac526af02a8066c04690469371a
E <e@elzr.me>
2025-08-11 11:01:25 -0400

    fix

--- src/database/instant.perms.ts
+++ src/database/instant.perms.ts
@@ -3,19 +3,17 @@
 import type { InstantRules } from "@instantdb/admin"
 
 const rules = {
+  attrs: {
+    allow: {
+      $default: "false",
+    },
+  },
   accounts: {
     allow: {
       $default: "IS_SELF",
     },
     bind: ["IS_SELF", "auth.id in data.ref('user.id')"],
   },
-  // TODO: REMOVE
-  // inboxes: {
-  //   allow: {
-  //     $default: "IS_OWNER",
-  //   },
-  //   bind: ["IS_OWNER", "auth.id in data.ref('owner.user.id')"],
-  // },
   scopes: {
     allow: {
       $default: "IS_OWNER",
@@ -26,25 +24,13 @@ const rules = {
     allow: {
       $default: "IS_OWNER_OF_SCOPE",
     },
-    bind: [
-      // TODO: REMOVE
-      // "IS_OWNER_OF_INBOX",
-      // "auth.id in data.ref('inbox.owner.user.id')",
-      "IS_OWNER_OF_SCOPE",
-      "auth.id in data.ref('scope.owner.user.id')",
-    ],
+    bind: ["IS_OWNER_OF_SCOPE", "auth.id in data.ref('scope.owner.user.id')"],
   },
   recurring_tasks: {
     allow: {
       $default: "IS_OWNER_OF_SCOPE",
     },
-    bind: [
-      // TODO: REMOVE
-      // "IS_OWNER_OF_INBOX",
-      // "auth.id in data.ref('inbox.owner.user.id')",
-      "IS_OWNER_OF_SCOPE",
-      "auth.id in data.ref('scope.owner.user.id')",
-    ],
+    bind: ["IS_OWNER_OF_SCOPE", "auth.id in data.ref('scope.owner.user.id')"],
   },
 } satisfies InstantRules
 
