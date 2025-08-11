2084a824f90c0f89ee02bd627202f0d17709ff9b
E <e@elzr.me>
2025-08-11 11:12:12 -0400

    metadata for current page

--- src/app/current/page.tsx
+++ src/app/current/page.tsx
@@ -1,2 +1,8 @@
+import { Metadata } from "next"
 import { CurrentTaskList } from "@/modules/task/current-tasks-list"
+
+export const metadata: Metadata = {
+  title: "Current Tasks",
+}
+
 export default CurrentTaskList
