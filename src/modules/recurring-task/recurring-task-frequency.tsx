a52b5f1c8686ac30d989c7a679663d27f48cd76d
E <e@elzr.me>
2025-08-11 21:50:04 -0400

    update recurring panel

--- src/modules/recurring-task/recurring-task-frequency.tsx
+++ src/modules/recurring-task/recurring-task-frequency.tsx
@@ -2,14 +2,14 @@
 
 import { useState } from "react"
 import { Heading } from "react-aria-components"
-import { CheckCheckIcon, CheckIcon, RefreshCwIcon } from "lucide-react"
+import { CheckIcon, RefreshCwIcon, SquareCheckIcon, SquareIcon } from "lucide-react"
 import { getRecurringTaskInfo, monthdayOptions, weekdayOptions } from "./recurring-task-info"
 import { RecurringTaskFrequencyValues } from "./recurring-task-info"
 import { RecurringTaskRecurDayType } from "@/database/models/recurring-task"
 import { ListBox, ListBoxItem } from "~/smui/list-box/components"
 import { Select, SelectButton, SelectPopover } from "~/smui/select/components"
 import { Modal } from "~/smui/modal/components"
-import { panel } from "@/common/components/class-names"
+import { panel, typography } from "@/common/components/class-names"
 import { cn } from "~/smui/utils"
 import { ToggleButton, ToggleButtonGroup } from "~/smui/toggle-button/components"
 import { Icon } from "~/smui/icon/components"
@@ -28,7 +28,7 @@ export function RecurringTaskFrequencyPicker({
 }) {
   const [values, setValues] = useState<RecurringTaskFrequencyValues>(initialValues)
 
-  const { label, frequency } = getRecurringTaskInfo(values)
+  const { label: frequencyLabel, frequency } = getRecurringTaskInfo(values)
 
   const { base, section } = panel()
 
@@ -51,15 +51,14 @@ export function RecurringTaskFrequencyPicker({
         slot="title"
         className={cn(section(), "flex items-center gap-8 p-4 px-8 font-medium")}
       >
-        <Icon icon={<RefreshCwIcon />} variants={{ size: "sm" }} />
-        {label}
+        Set Frequency...
       </Heading>
       <div className="bg-base-bg flex flex-col gap-4 rounded-sm p-4">
         <Select
           classNames={{
             button: {
               base: "cursor-pointer hover:bg-neutral-muted-bg flex items-center w-full p-8",
-              value: "grow text-left",
+              value: "grow text-left font-medium",
               icon: "size-14",
             },
             popover: "bg-base-bg rounded-sm border p-4 w-(--trigger-width)",
@@ -83,8 +82,8 @@ export function RecurringTaskFrequencyPicker({
               <SelectPopover className={classNames.popover}>
                 <ListBox
                   items={[
-                    { value: "weekday", label: "Choose weekdays..." },
-                    { value: "monthday", label: "Choose days of month..." },
+                    { value: "weekday", label: "Daily or Weekly" },
+                    { value: "monthday", label: "Monthly" },
                   ]}
                   classNames={{
                     base: [],
@@ -107,6 +106,55 @@ export function RecurringTaskFrequencyPicker({
         </Select>
         {values.recur_day_type === "weekday" && (
           <>
+            <div className="flex flex-col gap-4 border-y py-4">
+              <Button
+                className={[
+                  "flex items-center gap-4 px-8 py-4",
+                  frequency === "daily" ? "text-neutral-text" : "text-neutral-muted-text",
+                ]}
+                variants={{ hover: "fill" }}
+                onPress={() =>
+                  setValues({ recur_day_type: "weekday", recur_days: [0, 1, 2, 3, 4, 5, 6] })
+                }
+              >
+                <Icon icon={frequency === "daily" ? <SquareCheckIcon /> : <SquareIcon />} />
+                Daily
+              </Button>
+              <Button
+                className={[
+                  "flex items-center gap-4 px-8 py-4",
+                  frequency === "weekdays" ? "text-neutral-text" : "text-neutral-muted-text",
+                ]}
+                variants={{ hover: "fill" }}
+                onPress={() =>
+                  setValues({ recur_day_type: "weekday", recur_days: [1, 2, 3, 4, 5] })
+                }
+              >
+                <Icon icon={frequency === "weekdays" ? <SquareCheckIcon /> : <SquareIcon />} />
+                Every weekday
+              </Button>
+              <Button
+                className={cn([
+                  "flex items-center gap-4 px-8 py-4",
+                  frequency !== "daily" && frequency !== "weekdays"
+                    ? "text-neutral-text"
+                    : "text-neutral-muted-text",
+                ])}
+                variants={{ hover: "fill" }}
+                onPress={() => setValues({ recur_day_type: "weekday", recur_days: [1] })}
+              >
+                <Icon
+                  icon={
+                    frequency !== "daily" && frequency !== "weekdays" ? (
+                      <SquareCheckIcon />
+                    ) : (
+                      <SquareIcon />
+                    )
+                  }
+                />
+                Weekly (selected days)
+              </Button>
+            </div>
             <ToggleButtonGroup
               disallowEmptySelection
               selectionMode="multiple"
@@ -116,7 +164,7 @@ export function RecurringTaskFrequencyPicker({
               }}
               orientation="vertical"
               classNames={{
-                base: "flex flex-col items-stretch gap-2 border-y py-8",
+                base: "flex flex-col items-stretch gap-2",
                 button: [
                   "flex items-center justify-between px-8 py-4 gap-8",
                   "text-neutral-muted-text",
@@ -146,72 +194,57 @@ export function RecurringTaskFrequencyPicker({
                 </>
               )}
             </ToggleButtonGroup>
-            {frequency !== "daily" && (
-              <Button
-                className={[
-                  "flex items-center justify-between px-8 py-4",
-                  "text-neutral-text text-sm",
-                ]}
-                variants={{ hover: "fill" }}
-                onPress={() =>
-                  setValues({ recur_day_type: "weekday", recur_days: [0, 1, 2, 3, 4, 5, 6] })
-                }
-              >
-                Select every day
-                <Icon icon={<CheckCheckIcon />} variants={{ size: "sm" }} />
-              </Button>
-            )}
-            {frequency !== "weekdays" && (
-              <Button
-                className={[
-                  "flex items-center justify-between px-8 py-4",
-                  "text-neutral-text text-sm",
-                ]}
-                variants={{ hover: "fill" }}
-                onPress={() =>
-                  setValues({ recur_day_type: "weekday", recur_days: [1, 2, 3, 4, 5] })
-                }
-              >
-                Select all weekdays
-                <Icon icon={<CheckCheckIcon />} variants={{ size: "sm" }} />
-              </Button>
-            )}
           </>
         )}
         {values.recur_day_type === "monthday" && (
-          <ToggleButtonGroup
-            disallowEmptySelection
-            selectedKeys={values.recur_days}
-            selectionMode="multiple"
-            onSelectionChange={(selection) => {
-              setValues({ ...values, recur_days: [...selection] as number[] })
-            }}
-            classNames={{
-              base: "grid grid-cols-7 gap-2",
-              button: [
-                "rounded-sm border border-transparent py-2",
-                "text-neutral-muted-text/50",
-                "hover:text-base-text",
-                "data-selected:border-base-border data-selected:text-base-text data-selected:font-medium",
-              ],
-            }}
-          >
-            {(_, classNames) => (
-              <>
-                {monthdayOptions.map((value) => {
-                  return (
-                    <ToggleButton key={value} id={value} className={classNames.button}>
-                      {value}
-                    </ToggleButton>
-                  )
-                })}
-              </>
-            )}
-          </ToggleButtonGroup>
+          <>
+            <p className="text-neutral-muted-text px-4 pb-4 text-sm">Select days...</p>
+            <ToggleButtonGroup
+              disallowEmptySelection
+              selectedKeys={values.recur_days}
+              selectionMode="multiple"
+              onSelectionChange={(selection) => {
+                setValues({ ...values, recur_days: [...selection] as number[] })
+              }}
+              classNames={{
+                base: "grid grid-cols-7 gap-2",
+                button: [
+                  "rounded-sm border border-transparent py-2",
+                  "text-neutral-muted-text/50",
+                  "hover:text-base-text",
+                  "data-selected:border-base-border data-selected:text-base-text data-selected:font-medium",
+                ],
+              }}
+            >
+              {(_, classNames) => (
+                <>
+                  {monthdayOptions.map((value) => {
+                    return (
+                      <ToggleButton key={value} id={value} className={classNames.button}>
+                        {value}
+                      </ToggleButton>
+                    )
+                  })}
+                </>
+              )}
+            </ToggleButtonGroup>
+          </>
         )}
       </div>
-      <div className={section({ className: ["flex flex-col items-stretch"] })}>
-        <Button onPress={handleSave}>Save</Button>
+      <div className={section({ className: ["flex items-center", "p-4", "gap-4"] })}>
+        <Icon
+          icon={<RefreshCwIcon />}
+          variants={{ size: "sm" }}
+          className="text-neutral-muted-text"
+        />
+        <div className={typography({ type: "label" })}>{frequencyLabel}</div>
+        <div className="grow" />
+        <Button
+          onPress={handleSave}
+          className="bg-neutral-muted-bg rounded-sm border px-8 py-4 font-medium"
+        >
+          Save
+        </Button>
       </div>
     </Modal>
   )
--- src/modules/recurring-task/recurring-task-list/create-box.tsx
+++ src/modules/recurring-task/recurring-task-list/create-box.tsx
@@ -1,14 +1,11 @@
+"use client"
+
 import { useState } from "react"
 import { Group } from "react-aria-components"
-import { ArrowLeftRightIcon } from "lucide-react"
 import { RecurringTaskFrequencyPicker } from "../recurring-task-frequency"
-import { getRecurringTaskInfo } from "../recurring-task-info"
 import { RecurringTaskFrequencyValues } from "../recurring-task-info"
 import { CreateField } from "@/common/components/create-field"
 import { cn } from "~/smui/utils"
-import { Button } from "~/smui/button/components"
-import { typography } from "@/common/components/class-names"
-import { Icon } from "~/smui/icon/components"
 import { useSessionStorageUtility } from "@/common/utils/use-storage-utility"
 import { useCurrentScopeView } from "@/modules/scope/use-scope-views"
 import { parseRecurringTaskCreateInput } from "@/database/models/recurring-task"
@@ -17,23 +14,30 @@ import { db } from "@/database/db-client"
 export function RecurringTaskListCreateBox() {
   const { id: scopeId } = useCurrentScopeView()
   const [frequencyPickerOpen, setFrequencyPickerOpen] = useState(false)
-  const [frequency, setFrequency] = useSessionStorageUtility<RecurringTaskFrequencyValues>(
-    "recurring-task-default-frequency",
-    {
+
+  const [title, setTitle] = useState("")
+  const [defaultFrequency, setDefaultFrequency] =
+    useSessionStorageUtility<RecurringTaskFrequencyValues>("recurring-task-default-frequency", {
       recur_day_type: "weekday",
       recur_days: [0, 1, 2, 3, 4, 5, 6],
-    }
-  )
-  const handleCreate = async (title: string) => {
+    })
+
+  const handleTitleSubmit = async (title: string) => {
+    setTitle(title)
+    setFrequencyPickerOpen(true)
+  }
+
+  const handleFrequencySubmit = async (frequency: RecurringTaskFrequencyValues) => {
+    setDefaultFrequency(frequency)
     const { id, data, link } = parseRecurringTaskCreateInput({
       title,
       scope_id: scopeId,
       ...frequency,
     })
-    await db.transact(db.tx.recurring_tasks[id].link(link).create(data))
+    db.transact(db.tx.recurring_tasks[id].link(link).create(data))
+    setTitle("")
   }
 
-  const { label: frequencyLabel } = getRecurringTaskInfo(frequency)
   return (
     <>
       <Group
@@ -45,32 +49,21 @@ export function RecurringTaskListCreateBox() {
           "focus-within:border-l-neutral-border focus-within:border-l-4",
         ])}
       >
-        {({ isFocusWithin }) => (
+        {({}) => (
           <>
             <CreateField
-              onSubmit={handleCreate}
+              onSubmit={handleTitleSubmit}
               placeholder={`Add recurring task`}
               classNames={{ base: "!outline-0 grow !bg-transparent p-8 " }}
             />
-            <Button
-              onPress={() => setFrequencyPickerOpen(true)}
-              className={cn(
-                typography({ type: "label" }),
-                "flex items-center gap-4 px-8",
-                isFocusWithin || frequencyPickerOpen ? "visible" : "hidden"
-              )}
-            >
-              <Icon icon={<ArrowLeftRightIcon />} variants={{ size: "sm" }} />
-              {frequencyLabel}
-            </Button>
           </>
         )}
       </Group>
       <RecurringTaskFrequencyPicker
         isOpen={frequencyPickerOpen}
         onOpenChange={setFrequencyPickerOpen}
-        initialValues={frequency}
-        handleSaveValues={setFrequency}
+        initialValues={defaultFrequency}
+        handleSaveValues={handleFrequencySubmit}
       />
     </>
   )
