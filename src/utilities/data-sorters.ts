88d9712177313b4eb56910bafa2e1d7030254199
E <e@elzr.me>
2025-09-22 12:19:06 -0400

    consolidate sorter files

--- src/utilities/data-sorters.ts
+++ src/utilities/data-sorters.ts
@@ -1,4 +1,3 @@
-import { sortByListOrder } from "./sort-by-list-order"
 import { RecurringTask } from "@/database/models/recurring-task"
 import { Scope } from "@/database/models/scope"
 import { Task, TaskStatus } from "@/database/models/task"
@@ -73,4 +72,34 @@ export function sortScopes({ scopes, listOrder }: { scopes: Scope[]; listOrder: 
 // placeholder
 export function sortRtasks({ rtasks }: { rtasks: RecurringTask[] }) {
   return [...rtasks]
+}
+
+export function sortByListOrder<T extends object & { id: string }>({
+  items,
+  listOrder,
+  missingIdsPosition,
+  sortMissingIds,
+}: {
+  items: T[]
+  listOrder: string[]
+  missingIdsPosition?: "start" | "end"
+  sortMissingIds: (left: T, right: T) => number
+}): T[] {
+  const idSet = new Set(listOrder)
+  const sortedItems = items
+    .filter((item) => idSet.has(item.id))
+    .sort((a, b) => {
+      return listOrder.indexOf(a.id) - listOrder.indexOf(b.id)
+    })
+
+  const missingItems = items.filter((item) => !idSet.has(item.id))
+  if (missingItems.length === 0) return sortedItems
+
+  if (missingIdsPosition === "start") {
+    return [...missingItems.sort(sortMissingIds), ...sortedItems]
+  } else if (missingIdsPosition === "end") {
+    return [...sortedItems, ...missingItems.sort(sortMissingIds)]
+  }
+
+  return sortedItems
 }
